5d1c1f77f483a5ae308e283ee583136d97602f7b
Daniel Hunter <danielhunter19991506@gmail.com>
2026-03-29 12:25:19 +0400

    FIX: Delete dependcy of serde

--- src/component/policy.rs
+++ src/component/policy.rs
@@ -1,5 +1,3 @@
-use serde::Deserialize;
-
 #[derive(Debug, PartialEq)]
 pub enum RollingPolicy {
     Major,
@@ -18,35 +16,4 @@ pub enum FixedPolicy {
 pub enum UpdatePolicy {
     Fixed(FixedPolicy),
     Rolling(RollingPolicy)
-} 
-
-impl<'de> Deserialize<'de> for UpdatePolicy {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-        where
-            D: serde::Deserializer<'de> 
-    {
-        let s: String = Deserialize::deserialize(deserializer)?;
-
-        match s.split("::").collect::<Vec<_>>().as_slice() {
-            ["fixed", policy] => Ok(UpdatePolicy::Fixed(match *policy {
-                "LTS" => FixedPolicy::LTS,
-                "ESR" => FixedPolicy::ESR,
-                "frozen" => FixedPolicy::Frozen,
-                _ => return Err(serde::de::Error::custom(
-                    format!("unknown fixed policy: {}", policy)
-                )),
-            })),
-            ["rolling", policy] => Ok(UpdatePolicy::Rolling(match *policy {
-                "major" => RollingPolicy::Major,
-                "minor" => RollingPolicy::Minor,
-                "patch" => RollingPolicy::Patch,
-                _ => return Err(serde::de::Error::custom(
-                    format!("unknown rolling policy: {}", policy)
-                )),
-            })),
-            _ => Err(serde::de::Error::custom(
-                "expected format: 'fixed::POLICY' or 'rolling::POLICY'"
-            )),
-        }
-    }
 }
--- src/composition/dependcy.rs
+++ src/composition/dependcy.rs
@@ -1,12 +1,10 @@
 use std::collections::HashMap;
 
 use semver::VersionReq;
-use serde::Deserialize;
 
 use crate::component::policy::UpdatePolicy;
 
-#[derive(Debug, Deserialize)]
-#[serde(untagged)]
+#[derive(Debug)]
 pub enum Dependencies {
     Simple(VersionReq),
     Detailed(DepDetails)
@@ -15,22 +13,15 @@ pub enum Dependencies {
 pub type DepName = String;
 pub type DepHashMap = HashMap<DepName, Dependencies>;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 pub struct DepDetails {
     pub version: VersionReq,
     pub policy: UpdatePolicy,
-    #[serde(default)]
     pub path: Option<String>,
-    #[serde(default)]
     pub git: Option<String>,
-    #[serde(default)]
     pub branch: Option<String>,
-    #[serde(default)]
     pub features: Vec<String>,
-    #[serde(default)]
     pub optional: bool,
-    #[serde(default)]  // Полезно для указания конкретной версии из git
     pub rev: Option<String>,
-    #[serde(default)]  // Для указания тега
     pub tag: Option<String>,
 }
